15babf9f5c6c0d548403a93133f8ca38c78dd863
scapekambing <e.firmansyah@outlook.com>
2023-12-30 02:47:21 -0800

    IMP: infinite loop and fixed callback order

--- spray.py
+++ spray.py
@@ -2,6 +2,8 @@ import paho.mqtt.client as mqtt
 import threading
 import time
 
+TOPIC = "home/esp32e/turret"
+
 class User:
 
     def __init__(self, id, client, broker):
@@ -9,31 +11,40 @@ class User:
         self.client = client
         self.broker = broker
         self.is_spraying = False
-        self.stop_event = threading.Event()
+        self.done_spraying = True
+        self.active = True
 
     def connect(self):
         self.client.connect(self.broker)
+        self.client.subscribe(TOPIC)  # Subscribe to the switch topic
+        self.client.on_message = self.on_message  # Set the on_message callback
 
-    def send(self, msg):
-        if msg == "spray":
+    def on_message(self, client, userdata, msg):
+        message = msg.payload.decode("utf-8")
+
+        if message == "spray":
             print("Spraying... \U0001F608")
             self.is_spraying = True
-        elif msg == "stop":
+        elif message == "stop":
             print("Stopping... \U0001F607")
             self.is_spraying = False
+        elif message == "end":
+            print("Spraying complete. \U0001F60E")
+            self.is_spraying = False
+            self.active = False
         else:
-            print("Invalid input. Valid inputs are 'spray' or 'stop'.")
+            print("Invalid input. Valid inputs are 'spray', 'stop' or 'end'.")
+        
+        self.done_spraying = True
 
-        self.client.publish("esp32e-switch/switch", str(msg))
-
-    def input_timer(self):
-        time.sleep(5)
-        self.stop_event.set()  # Set the event to signal input timeout
+    def send(self, msg):
+        self.client.publish(TOPIC, str(msg))
+        self.done_spraying = False
 
     def disconnect(self):
         if self.is_spraying:
-            print("Stopping spray before disconnecting... \U0001F607")
-            self.client.publish("esp32e-switch/switch", "stop")
+            print("Stopping spray before disconnecting...")
+            self.send("stop")
             time.sleep(1)
         self.client.disconnect()
 
@@ -46,21 +57,17 @@ def main():
 
     try:
         user.connect()
+        user.client.loop_start()  # Start the MQTT client's threaded loop
 
-        # Create and start threads
-        input_thread = threading.Thread(target=user.input_timer)
-        input_thread.start()
+        while (user.active):
+            if(user.done_spraying):
+                msg = input("Enter 'spray' to start spraying, 'stop' to stop spraying or 'end' to terminate the program: ")
+                user.send(msg)
 
-        while not user.stop_event.is_set():
-            msg = input("Enter 'spray' to start spraying or 'stop' to stop spraying: ")
-            user.send(msg)
-
-        print("Timeout. Disconnecting and stopping spray...")
     except KeyboardInterrupt:
         print("\nKeyboard interrupt detected. Exiting gracefully...")
     finally:
         user.disconnect()
-        input_thread.join()
         print("Program exited gracefully.")
 
 if __name__ == "__main__":
